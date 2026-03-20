1e971f4749368b1cfc5317ce7f47907972724efd
JeremyRhain <schmidtjeremyr@gmail.com>
2026-03-20 13:05:57 -0700

    BSC4-app rename updates

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,9 +4,3 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
 });
-
-/* ----- REMOVED FOR DEPLOYMENT ----- */
-// base:
-//   process.env.NODE_ENV === "production"
-//     ? "/TrpTn-Final-Project-Frontend/"
-//     : "/",
