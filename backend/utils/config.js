9b1a5cbc60d02efab7d9391050c29d4ff4f6b602
RhainUnity <schmidtjeremyr@gmail.com>
2026-04-02 20:49:40 -0700

    db fix

--- backend/utils/config.js
+++ backend/utils/config.js
@@ -2,7 +2,7 @@
 
 const { NODE_ENV, JWT_SECRET = "dev-secret", MONGO_URL } = process.env;
 
-const DEFAULT_MONGO_URL = "mongodb://127.0.0.1:27017/shopping_db";
+const DEFAULT_MONGO_URL = "mongodb://127.0.0.1:27017/easy_budget_shopping_app_db";
 
 module.exports = {
   JWT_SECRET: NODE_ENV === "production" ? JWT_SECRET : "dev-secret",
